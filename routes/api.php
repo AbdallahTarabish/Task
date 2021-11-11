ef7a129f7c6b1cbf0a0255488445c1285322b9a6
AbdallahTarabish <dev.abdallah.tarabish@gmail.com>
2021-11-11 10:30:00 +0200

    add re-order route

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(["prefix" => "user", "as" => "user.", "middleware" => "auth:api"], function () {
     Route::resource('task', TaskController::class);
+    Route::put("task/re-order", "\App\Http\Controllers\TaskController@reorderPriority");
 });
 Route::post("login", "\App\Http\Controllers\UserAuthController@login");
